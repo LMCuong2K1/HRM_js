aaeea1bf0be28678461e75e2a66354e915083aed
LMCuong2211 <cuong22112001@gmail.com>
2025-10-15 15:36:28 +0700

    html form

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-import AuthMobile from "./authModule";
+import AuthMobile from "./authModule.js";
 console.log("AuthMobile:",AuthMobile);
+
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
+const dashboard = document.getElementById('dashboard');
+const userDisplay = document.getElementById('userDisplay');
+const logoutBtn = document.getElementById('logoutBtn');
